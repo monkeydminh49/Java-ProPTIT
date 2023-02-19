a5566ed8a660f25cdb62eb4221c3abffc12f0d95
Nguyen Dang Minh <90394315+monkeydminh49@users.noreply.github.com>
2023-02-19 15:06:14 +0700

    Add files via upload

---
+++ Quan_li_giang_vien.java
@@ -0,0 +1,10 @@
+public class Quan_li_giang_vien {
+
+  public static void main(String[] args) {
+    Teacher teacher = new Teacher();
+    teacher.setName("Minh");
+    System.out.println(teacher.getName());
+  }
+}
+
+
